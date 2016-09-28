5bc8e4128bbb698a333b90dafa1df787c9faf913
huangxuan <770493410@qq.com>
2016-09-28 11:02:45 +0800

    Initial Commit

---
+++ ThirdLoginDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThirdLoginDemo
+//
+//  Created by 黄轩 on 16/9/28.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThirdLoginDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThirdLoginDemo
+//
+//  Created by 黄轩 on 16/9/28.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
